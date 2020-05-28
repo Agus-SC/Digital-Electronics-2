d08e797e8a629a1d0e6f3ea20ee937084466decd
Agustin Mamone <agustinfmamone@gmail.com>
2020-05-28 17:33:30 -0300

    Vimos el codigo i2cm_interrupt

--- I2C_handler/Proyecto_final.c
+++ I2C_handler/Proyecto_final.c
@@ -73,17 +73,51 @@ typedef struct {
 /*Masks*/
 /*#define MASK_MASK(n) (n & 0xFE)*/
 
+// Estructura para Master Mode.
+/* uint16_t Porque quizas se necesita usar un buffer ya se para transmitir
+o recibir de mas de 2^8 = 256 elementos */
 typedef struct{
-    uint8_t SLAVE_ADDR;
-    uint8_t *Tx_BUFFER;
-    int Tx_SIZE; 
-}MasterTransfer_TMsg;
+    uint8_t SLAVE_ADDR; /* Le paso el slave address */
+    uint8_t OPTIONS;	/* Option, todavia no sabemos */
+	uint8_t STATUS;		/* Estado de transmision */
+	uint16_t Tx_SIZE;		/* Tama;o del buffer a transmitir (Numero de bytes), Transmito el Address si recivo */
+	uint16_t Rx_SIZE;		/* Tama;o del buffer a recibir (Numero de bytes), 0 si transmito*/
+	uint8_t *Tx_BUFFER; /* Puntero al buffer de datos a transmitir, NULL si recibo */
+	uint8_t *Rx_BUFFER;
+     
+}MasterTransfer_T;
 
-typedef struct{
-    uint8_t *Rx_BUFFER;
-    int Rx_SIZE; 
-}MasterTransfer_RMsg;
+void I2CM_Xfer(LPC_I2C_T *pI2C, I2CM_XFER_T *xfer)
+{
+	/* set the transfer status as busy */
+	xfer->status = I2CM_STATUS_BUSY;
+	/* Clear controller state. */
+	Chip_I2CM_ResetControl(pI2C);
+	/* Enter to Master Transmitter mode */
+	Chip_I2CM_SendStart(pI2C);
+}
 
-bool I2C_Master_Transfer(){
+/**
+ * @brief	Reset I2C controller state
+ * @param	pI2C	: Pointer to selected I2C peripheral
+ * @return	Nothing
+ * @note	This function clears all control/status flags.
+ */
+void I2CM_ResetControl(LPC_I2C_T *pI2C)
+{
+	/* Reset STA, AA and SI. Stop flag should not be cleared as it is a reserved bit */
+	pI2C->CONCLR = I2C_CON_SI | I2C_CON_STA | I2C_CON_AA;
 
 }
+/**
+ * @brief	Transmit START or Repeat-START signal on I2C bus
+ * @param	pI2C	: Pointer to selected I2C peripheral
+ * @return	Nothing
+ * @note	This function sets the controller to transmit START condition when
+ *          the bus becomes free.
+ */
+void I2CM_SendStart(LPC_I2C_T *pI2C)
+{
+	pI2C->CONSET = I2C_CON_I2EN | I2C_CON_STA;
+}
+
