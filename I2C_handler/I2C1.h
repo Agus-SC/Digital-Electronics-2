066bae3b3128b66080494804f968bd5df0096df6
Agustin Mamone <agustinfmamone@gmail.com>
2020-07-13 19:22:54 -0300

    planteamos las rxslave y txslave

--- I2C_handler/I2C1.h
+++ I2C_handler/I2C1.h
@@ -38,8 +38,6 @@ typedef struct {
     
 } MSG_T ;
 
-
-
 typedef enum {
 	I2C_STATUS_DONE,	/**< Transfer done successfully */
 	I2C_STATUS_NAK,		/**< NAK received during transfer */
@@ -75,6 +73,14 @@ typedef enum {
 /* Mascara para leer el dato del registro DAT 0:7 */
 #define MASK_DAT            0xFF // 11111111
 
+//funcion que devuelve 0 para interfaz I2C0 o 1 para interfaz I2C1
+_bool get_interface(uint8_t interface){
+    if (interface == 0){
+        return false ;
+    }
+    return true ;
+} 
+
 // Funcion que inicializa el registro CONSET
 void ENA_interface(I2C_T *pI2C){
     pI2C -> CONCLR = CONCLR_I2EN | CONCLR_STA | CONCLR_SI | CONCLR_AA ;
@@ -93,15 +99,36 @@ void STOP_send(I2C_T *pI2C){
 
 /* Funcion que limpia el flag SI */
 void SI_clear(I2C_T *pI2C){
-    pI2C -> CONCLR = CONCLR_SI ;
+    pI2C -> CONCLR |= CONCLR_SI ;
 }
 
 uint32_t get_STATUS(I2C_T *pI2C){
     return (uint32_t) pI2C -> STAT & MASK_STAT ;
 } 
 
+void set_SLAVEADDR(I2C_T *pI2C, uint8_t SLAVE_ADDRESS){
+    int i ; 
+    pI2C -> ADR0 = SLAVE_ADDRESS << 1 ;
+    /* duplicamos */ 
+    pI2C -> ADR1 = SLAVE_ADDRESS << 1 ;
+    pI2C -> ADR2 = SLAVE_ADDRESS << 1 ;
+    pI2C -> ADR3 = SLAVE_ADDRESS << 1 ;
+    /* Seteamos la mascara */
+    pI2C -> MASK[0] = SLAVE_ADDRESS ;
+    for(i = 1 ; i < 4 ; i++){
+        pI2C -> MASK[i] = 0x00 ;
+    } 
+}
+
+void init_SLAVE(I2C_T *pI2C, uint8_t SLAVE_ADDRESS){
+    set_SLAVEADDR(pI2C, SLAVE_ADDRESS) ;
+    pI2C ->CONCLR = CONCLR_STA | CONCLR_SI ;
+    pI2C -> CONSET = CONSET_I2EN | CONSET_AA ;
+}
+
+
 /* Funcion que transmite n byte en master mode */
-uint32_t WRITE_nbyte(I2C_T *pI2C, MSG_T MSG){
+uint32_t Tx_master(I2C_T *pI2C, MSG_T MSG){
     ENA_interface(pI2C) ;
     START_send(pI2C) ;
     while(MSG.TxMSG_SIZE > 0){
@@ -173,7 +200,7 @@ uint32_t WRITE_nbyte(I2C_T *pI2C, MSG_T MSG){
 } 
 
 /* Funcion que recibe n byte en master mode */
-uint32_t READ_nbyte(I2C_T *pI2C, MSG_T MSG){
+uint32_t Rx_master(I2C_T *pI2C, MSG_T MSG){
     ENA_interface(pI2C) ;
     START_send(pI2C) ;
     while(MSG.RxMSG_SIZE > 0){
@@ -225,7 +252,7 @@ uint32_t READ_nbyte(I2C_T *pI2C, MSG_T MSG){
             *MSG.RxMSG_DATA++ = ( pI2C -> DAT ) & MASK_DAT ;
             if (MSG.RxMSG_SIZE == 1){
                 MSG.RxMSG_SIZE-- ;
-                pI2C -> CONCLR = CONCLR_AA  ; // set STO and AA
+                pI2C -> CONCLR = CONCLR_AA  ; // clear AA
                 SI_clear(pI2C) ; // clear SI
                 break ;
             }
@@ -244,4 +271,128 @@ uint32_t READ_nbyte(I2C_T *pI2C, MSG_T MSG){
         }
     }
     return I2C_STATUS_DONE ;
+}
+
+void Rx_slave(I2C_T *pI2C, MSG_T MSG){
+
+    while (MSG.RxMSG_SIZE > 0){    
+        switch(get_STATUS(pI2C)){
+            /* Reciver states */
+            case 0x60:
+                /* Own Slave Address + Write has been received, ACK has been returned. Data will be
+                received and ACK returned. */
+                pI2C -> CONSET = CONSET_AA ; // set AA bits
+                SI_clear(pI2C) ; // clear SI flag
+                // 3. Set up Slave Receive mode data buffer.
+                // 4. Initialize Slave data counter.
+                break ;
+
+            case 0x68:
+                /* Arbitration has been lost in Slave Address and R/W bit as bus Master. Own Slave Address
+                + Write has been received, ACK has been returned. Data will be received and ACK will be
+                returned. STA is set to restart Master mode after the bus is free again. */
+            case 0x78:
+                /* Arbitration has been lost in Slave Address + R/W bit as bus Master. General call has been
+                received and ACK has been returned. Data will be received and ACK returned. STA is set
+                to restart Master mode after the bus is free again. */
+                pI2C -> CONSET = CONSET_STA | CONSET_AA ;  // set STA and the AA bit
+                SI_clear(pI2C) ; // clear SI flag 
+                // 3. Set up Slave Receive mode data buffer.
+                // 4. Initialize Slave data counter.
+                break ;
+
+            case 0x70:
+                /*General call has been received, ACK has been returned. Data will be received and ACK
+                returned.*/
+                pI2C -> CONSET = CONSET_AA ; // set the AA bit
+                SI_clear(pI2C) ; // clear SI flag  
+                // 3. Set up Slave Receive mode data buffer.
+                // 4. Initialize Slave data counter.
+                break ;
+                
+            case 0x80:
+                /* Previously addressed with own Slave Address. Data has been received and ACK has
+                been returned. Additional data will be read. */
+                *MSG.RxMSG_DATA++ = ( pI2C -> DAT ) & MASK_DAT ;
+                if (MSG.RxMSG_SIZE == 1){
+                    MSG.RxMSG_SIZE-- ;
+                    pI2C -> CONCLR = CONCLR_AA  ; // clear AA
+                    SI_clear(pI2C) ; // clear SI flag
+                    break ;
+                }
+                MSG.RxMSG_SIZE-- ;
+                pI2C -> CONSET = CONSET_AA ; // set AA bits
+                SI_clear(pI2C) ; // clear SI flag
+                break ;
+
+            case 0x88:
+                /* Previously addressed with own Slave Address. Data has been received and NOT ACK
+                has been returned. Received data will not be saved. Not addressed Slave mode is
+                entered. */
+                pI2C -> CONSET = CONSET_AA ; // set AA bits
+                SI_clear(pI2C) ; // clear SI flag
+                break ;
+
+            case 0x90:
+                /* Previously addressed with General Call. Data has been received, ACK has been returned.
+                Received data will be saved. Only the first data byte will be received with ACK. Additional
+                data will be received with NOT ACK. */
+                *MSG.RxMSG_DATA = ( pI2C -> DAT ) & MASK_DAT ;
+                pI2C -> CONCLR = CONCLR_AA  ; // clear AA
+                SI_clear(pI2C) ; // clear SI flag
+                break ;
+
+            case 0x98:
+                /* Previously addressed with General Call. Data has been received, NOT ACK has been
+                returned. Received data will not be saved. Not addressed Slave mode is entered. */
+            case 0xA0:
+                /* A STOP condition or Repeated START has been received, while still addressed as a
+                Slave. Data will not be saved. Not addressed Slave mode is entered. */
+                pI2C -> CONSET = CONSET_AA ; // set AA bits
+                SI_clear(pI2C) ; // clear SI flag
+                break ;
+        }
+    }
+        
+}        
+         
+void Tx_slave(I2C_T *pI2C, MSG_T MSG){
+
+    while (MSG.TxMSG_SIZE > 0){    
+        switch(get_STATUS(pI2C)){        
+         /* Transmitter states */
+        case 0xA8:
+            /* Own Slave Address + Read has been received, ACK has been returned. Data will be
+            transmitted, ACK bit will be received. */
+        case 0xB8:
+            /* Data has been transmitted, ACK has been received. Data will be transmitted, ACK bit will
+            be received. */
+            pI2C -> DAT = ( *MSG.TxMSG_DATA++ ) ;
+            MSG.TxMSG_SIZE-- ;  
+            pI2C -> CONSET = CONSET_AA ; // set AA bits
+            SI_clear(pI2C) ; // clear SI flag
+            break ;
+
+        case 0xB0:
+            /* Arbitration lost in Slave Address and R/W bit as bus Master. Own Slave Address + Read
+            has been received, ACK has been returned. Data will be transmitted, ACK bit will be
+            received. STA is set to restart Master mode after the bus is free again. */
+            pI2C -> DAT = ( *MSG.TxMSG_DATA++ ) ;
+            MSG.TxMSG_SIZE-- ;  
+            pI2C -> CONSET = CONSET_STA | CONSET_AA ;  // set STA and the AA bit
+                SI_clear(pI2C) ; // clear SI flag 
+            break ;
+
+        case 0xC0:
+            /* Data has been received, NOT ACK has been returned. Data will be read from DAT.
+            A STOP condition will be transmitted. */
+        case 0xC8:
+            /* The last data byte has been transmitted, ACK has been received. Not addressed Slave
+            mode is entered. */
+            pI2C -> CONSET = CONSET_AA ; // set AA bits
+            SI_clear(pI2C) ; // clear SI flag
+            break ;
+        }
+    }
+
 }
